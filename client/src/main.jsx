0acdbfac511e6445aa9f2bf7d6f89283b54ff4cf
Partha <parthasdeyofficial@gmail.com>
2024-04-27 22:59:27 +0530

    initial commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Routes from './Router.jsx'
+import Routes from './Routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
