eea17d2d9f7eaa277b54af345c487102fcadb135
Partha <parthasdeyofficial@gmail.com>
2024-04-27 12:14:12 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Navbar />
+
+      {/* hero section */}
+      <div className="w-full h-[900px] bg-gray-900 flex-col justify-center">
+          <div className="w-full h-[300px] flex justify-center items-center font-['Merriweather'] md:pt-32">
+            <span className='text-5xl text-white text-center md:leading-relaxed font-semibold'>Revolutionizing Brain Tumour <br /> Detection</span>
+          </div>
+
+          <div className="w-full h-[50px] flex justify-center items-center font-['Poppins'] -mt-6 md:pt-6">
+            <span className='text-white text-md text-center'>Cutting Edge AI Technologies for accurate Diagnosis!</span>
+          </div>
+      </div>
+      
+      <Footer />
     </>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,22 +2,27 @@ import 'remixicon/fonts/remixicon.css'
 
 function Navbar() {
   return (
-    <div className="w-full h-[70px] md:h-[80px] bg-blue-600 flex justify-between p-4">
-        <div>
-            <span className="text-white text-4xl md:text-5xl font-semibold font-['Jersey']">nvAI</span>
+    <div className="w-full h-[70px] md:h-[80px] bg-gray-900 flex justify-between p-4">
+        <div className="md:px-4">
+            <a href='/' className="text-white text-4xl md:text-5xl font-semibold font-['Jersey']">nvAI</a>
         </div>
 
         <div className="hidden md:block pt-2">
             <ul className="flex justify-center">
-                <li className="text-white text-xl font-semibold px-4 py-1 hover:bg-white/30 duration-300 hover:duration-300 rounded"><a href="/">Home</a></li>
-                <li className="text-white text-xl font-semibold px-4 py-1 hover:bg-white/30 duration-300 hover:duration-300 rounded"><a href="/detection">Detection</a></li>
-                <li className="text-white text-xl font-semibold px-4 py-1 hover:bg-white/30 duration-300 hover:duration-300 rounded"><a href="https://github.com/parthasdey2304/nvAI.git">GitHub</a></li>
-                <li className="text-white text-xl font-semibold px-4 py-1 hover:bg-white/30 duration-300 hover:duration-300 rounded"><a href="/about">About</a></li>
+                <li className="text-white text-xl font-medium px-4 py-1 hover:bg-white/30 duration-300 hover:duration-300 rounded font-['Poppins']"><a href="/">Home</a></li>
+                <li className="text-white text-xl font-medium px-4 py-1 hover:bg-white/30 duration-300 hover:duration-300 rounded font-['Poppins']"><a href="/detection">Detection</a></li>
+                <li className="text-white text-xl font-medium px-4 py-1 hover:bg-white/30 duration-300 hover:duration-300 rounded font-['Poppins']"><a href="https://github.com/parthasdey2304/nvAI.git">GitHub</a></li>
+                <li className="text-white text-xl font-medium px-4 py-1 hover:bg-white/30 duration-300 hover:duration-300 rounded font-['Poppins']"><a href="/about">About</a></li>
             </ul>
         </div>
 
         <div className="md:hidden">
-            <i class="ri-menu-3-line text-white font-bold text-3xl hover:bg-white/30 duration-300 hover:duration-300 p-1 rounded"></i>
+            <i class="ri-menu-line text-white font-bold text-3xl hover:bg-white/30 duration-300 hover:duration-300 p-1 rounded"></i>
+        </div>
+
+        <div className='flex space-x-4 md:pt-2 md:pr-4'>
+            <a href="/login" className="text-white text-xl font-medium px-4 py-1 hover:bg-white/30 duration-300 hover:duration-300 rounded font-['Poppins']">Login</a>
+            <a href="/register" className="hidden md:block text-white text-xl font-medium px-4 py-1 bg-cyan-600 duration-300 hover:duration-300 rounded font-['Poppins']">Register</a>
         </div>
     </div>
   )
