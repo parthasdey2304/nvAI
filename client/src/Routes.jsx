30ceb3b3eb680670b863b88b842834751989910e
Partha <parthasdeyofficial@gmail.com>
2024-04-27 23:02:20 +0530

    initial commit

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage.jsx';
 import Detection from './pages/Detection.jsx';
 import About from './pages/About.jsx';
@@ -8,13 +8,11 @@ import Register from './pages/Register.jsx';
 const Routes = () => {
   return (
     <Router>
-      <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/detection" component={Detection} />
         <Route path="/about" component={About} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-      </Switch>
     </Router>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Routes from './Routes.jsx'
+// import Routes from './Routes.jsx'
+import Homepage from './pages/Homepage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Routes />
+    {/* <Routes /> */}
+    <Homepage />
   </React.StrictMode>,
 )
