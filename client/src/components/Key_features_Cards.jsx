7b50b13359d6ed588a037fe0593f54c93aa4a334
Partha <parthasdeyofficial@gmail.com>
2024-05-04 02:48:07 +0530

    Update ChatBot.jsx

--- client/src/components/Key_features_Cards.jsx
+++ client/src/components/Key_features_Cards.jsx
@@ -3,10 +3,10 @@ function Key_features_Cards({ title, info}) {
     <div className="w-[90%] md:w-[400px] h-[250px] bg-white/30 flex-col space-y-5 p-4 pt-12 rounded-md font-['Poppins']">
         <div className="flex space-x-5">
             <span className="material-symbols-outlined text-3xl text-white">neurology</span>
-            <span className="text-white font-bold text-2xl">{ title }</span>
+            <span className="text-white font-medium text-2xl">{ title }</span>
         </div>
 
-        <div className='text-white font-semibold pl-10 text-justify pr-5'>{ info }</div>
+        <div className='text-white pl-10 text-justify pr-5'>{ info }</div>
     </div>
   )
 }
