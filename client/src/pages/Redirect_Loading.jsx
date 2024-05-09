2adabc40b202b1930c2402440f10efdbffa608f5
Partha <parthasdeyofficial@gmail.com>
2024-05-09 12:17:08 +0530

    Update : Chatbot.jsx (chatbot component out of screen issue solved)

--- client/src/pages/Redirect_Loading.jsx
+++ client/src/pages/Redirect_Loading.jsx
@@ -10,7 +10,7 @@ const LoadingScreen = ({ redirectTo }) => {
 
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      <Link to="/" className="ri-arrow-left-line absolute font-bold text-3xl top-1 left-1 md:top-5 md:left-5 hover:bg-black duration-300 hover:duration-300 hover:text-white px-2.5 py-2 rounded-full"></Link>
+      <Link to="/" className="ri-arrow-left-line absolute font-bold text-3xl top-1 left-1 md:top-5 md:left-5 text-white hover:bg-white duration-300 hover:duration-300 hover:text-black px-2.5 py-2 rounded-full"></Link>
       <div className='w-[50px] md:w-[100px] h-[50px] md:h-[100px] animate-spin rounded-full border-8 border-t-blue-600'>
       </div>
     </div>
