0c56b0196f4034b0b02f973152ba7cddf48c4a63
Partha <parthasdeyofficial@gmail.com>
2024-05-09 11:52:40 +0530

    Update : Redirect_Loading.jsx(added back button)

--- client/src/pages/Redirect_Loading.jsx
+++ client/src/pages/Redirect_Loading.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const LoadingScreen = ({ redirectTo }) => {
   // useEffect(() => {
@@ -10,7 +10,8 @@ const LoadingScreen = ({ redirectTo }) => {
 
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      <div className='w-[100px] h-[100px] animate-spin rounded-full border-8 border-t-blue-600'>
+      <Link to="/" className="ri-arrow-left-line absolute font-bold text-3xl top-1 left-1 md:top-5 md:left-5 hover:bg-black duration-300 hover:duration-300 hover:text-white px-2.5 py-2 rounded-full"></Link>
+      <div className='w-[50px] md:w-[100px] h-[50px] md:h-[100px] animate-spin rounded-full border-8 border-t-blue-600'>
       </div>
     </div>
   );
