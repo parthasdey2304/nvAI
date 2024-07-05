f81cb6e9a5881a5218f2803fd70e7c4e55e77fb3
Partha <parthasdeyofficial@gmail.com>
2024-07-06 01:00:37 +0530

    Update: Detection.jsx server link changed!

--- client/src/pages/Detection.jsx
+++ client/src/pages/Detection.jsx
@@ -14,7 +14,7 @@ const animationVariants = {
   visible: { opacity: 1, y: 0 },
 };
 
-const serverLink = "https://5444-13-60-94-47.ngrok-free.app/";
+const serverLink = "https://dd23-157-40-74-6.ngrok-free.app/";
 
 function Detection() {
   const [selectedImage, setSelectedImage] = useState(null);
